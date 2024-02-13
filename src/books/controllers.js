95e397bc400d8b51345e687c8f19c213e4472d0b
Andrew Cookson <andrewcookson@email.com>
2024-02-13 17:21:50 +0000

    enhancement: added getAllBooks, updateAuthor & deleteBookByTitle

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -14,6 +14,57 @@ const addBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.status(200).json({ books: books });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const updateAuthor = async (req, res) => {
+  try {
+    const { title, author } = req.body;
+
+    const book = await Book.findOne({ where: { title: title } });
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    book.author = author;
+    await book.save();
+
+    res.status(200).json({ message: "Author updated", book: book });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const deleteBookByTitle = async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const book = await Book.findOne({ where: { title: title } });
+
+    if (!book) {
+      return res
+        .status(404)
+        .json({ error: "No book found with the specified title" });
+    }
+
+    await book.destroy();
+
+    res.status(200).json({ message: "Book deleted", deletedBook: book });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
 module.exports = {
   addBook: addBook,
+  getAllBooks: getAllBooks,
+  updateAuthor: updateAuthor,
+  deleteBookByTitle: deleteBookByTitle,
 };
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
 const bookRouter = Router();
 
-const { addBook } = require("./controllers");
+const {
+  addBook,
+  getAllBooks,
+  updateAuthor,
+  deleteBookByTitle,
+} = require("./controllers");
 
 bookRouter.post("/books/addBook", addBook);
+bookRouter.get("/books/getAllBooks", getAllBooks);
+bookRouter.put("/books/updateAuthor", updateAuthor);
+bookRouter.delete("/books/deleteBookByTitle", deleteBookByTitle);
 
 module.exports = bookRouter;
