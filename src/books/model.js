9b8b217dce07af99f43eae7a51e96100ddc75ce0
Andrew Cookson <andrewcookson@email.com>
2024-02-13 14:42:28 +0000

    enhancement: add post route

--- src/books/model.js
+++ src/books/model.js
@@ -1,23 +1,25 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 const sequelize = require("../db/connection");
 
-const Book = sequelize.define("Book", {
+const Book = sequelize.define(
+  "Book",
+  {
     title: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
     },
     author: {
-        type: DataTypes.STRING,
-        defaultValue: "some author",
+      type: DataTypes.STRING,
+      defaultValue: "some author",
     },
     genre: {
-        type: DataTypes.STRING,
-        defaultValue: "some genre",
+      type: DataTypes.STRING,
+      defaultValue: "some genre",
     },
-},
-{ timestamps: false }
+  },
+  { timestamps: false }
 );
 
 module.exports = Book;
--- src/server.js
+++ src/server.js
@@ -3,22 +3,25 @@ const express = require("express");
 
 const Book = require("./books/model");
 
+const bookRouter = require("./books/routes");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
 
+app.use(bookRouter);
+
 const syncTables = async () => {
-    await Book.sync();
+  await Book.sync();
 };
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ message: "API is healthy"})
+  res.status(200).json({ message: "API is healthy" });
 });
 
 app.listen(port, () => {
-    syncTables();
-console.log(`Server is listening on port ${port}`);
-})
-
+  syncTables();
+  console.log(`Server is listening on port ${port}`);
+});
