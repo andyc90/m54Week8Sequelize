76021cca86e53decb323fac96e88f1903df6d461
Andrew Cookson <andrewcookson@email.com>
2024-02-14 17:52:25 +0000

    enhancement: deleted unnecessary comments

--- src/authors/controllers.js
+++ src/authors/controllers.js
@@ -1,4 +1,3 @@
-// This file contains the functions for the authors routes
 const Author = require("./model");
 
 // Adds an author to the database
--- src/genres/controllers.js
+++ src/genres/controllers.js
@@ -1,4 +1,3 @@
-// This file contains the functions for the genres routes
 const Genre = require("./model");
 
 // Adds a genre to the database
